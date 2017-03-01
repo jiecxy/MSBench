f7ee3c3a0c620512a455d99ed9e60a1d7b48c0dd
jiecxy <apple@Jiecxy-2.local>
2017-03-01 17:04:09 +0800

    init

---
+++ core/src/main/java/cn/ac/ict/Client.java
@@ -0,0 +1,7 @@
+package cn.ac.ict;
+
+/**
+ * Created by apple on 2017/3/1.
+ */
+public class Client {
+}
---
+++ core/src/main/java/cn/ac/ict/MS.java
@@ -0,0 +1,52 @@
+package cn.ac.ict;
+
+import java.util.Properties;
+
+/**
+ * Created by apple on 2017/3/1.
+ */
+public abstract class MS {
+
+    /**
+     * Properties for configuring this MSCLient.
+     */
+    private Properties properties = new Properties();
+
+    /**
+     * Set the properties for this MSCLient.
+     */
+    public void setProperties(Properties p) {
+        properties = p;
+
+    }
+
+    /**
+     * Get the set of properties for this MSCLient.
+     */
+    public Properties getProperties() {
+        return properties;
+    }
+
+    /**
+     * Initialize any state for this MSCLient.
+     * Called once per MSCLient instance; there is one MSCLient instance per client thread.
+     */
+    public void init() throws MSException {
+    }
+
+    /**
+     * Send a record to the Message System.
+     *
+     * @param msg The message to be sent
+     * @return
+     */
+    public abstract Status send(String msg);
+
+    /**
+     * read messages from the Message System.
+     *
+     * @param
+     * @return
+     */
+    public abstract Status read();
+}
