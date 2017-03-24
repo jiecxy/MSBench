90f977e76a2b2baf232b7e5c38fe3a5079bc3020
krumo <whrzxzero@gmail.com>
2017-03-24 03:08:52 -0400

    add WorkerCallBack

--- core/src/main/java/cn/ac/ict/communication/WorkerCallBack.java
+++ core/src/main/java/cn/ac/ict/communication/WorkerCallBack.java
@@ -3,5 +3,7 @@ package cn.ac.ict.communication;
 /**
  * Created by krumo on 3/24/17.
  */
-public class WorkerCallBack {
+public interface WorkerCallBack {
+    public void handleSentMessage(byte[] msg);
+    public void handleReceivedMessage(byte[] msg);
 }
